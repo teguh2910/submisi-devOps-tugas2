7c7217d62567e16436651fd8096dd0b494c0c02a
teguh2910 <teguh.yuhono@student.unsia.ac.id>
2023-05-31 23:54:39 +0700

    first commit

---
+++ karsajobs-ui/build_push_image_karsajobs_ui.sh
@@ -0,0 +1,3 @@
+docker build -t teguhy2910/karsajobs-ui:latest .
+docker login
+docker push teguhy2910/karsajobs-ui:latest
---
+++ karsajobs/build_push_image_karsajobs.sh
@@ -0,0 +1,3 @@
+docker build -t teguhy2910/karsajobs:latest .
+docker login
+docker push teguhy2910/karsajobs:latest
